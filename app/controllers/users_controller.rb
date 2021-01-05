9de247b8dd87271720e503db6db0d8446026c6a6
Haloren <mckennakyler@gmail.com>
2021-01-04 22:04:05 -0700

    set up user controller index & create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all
+        render json: users, include: [:weather, :todos, :events]
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {message: user.errors.full_messages.to_sentence}
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
 end
