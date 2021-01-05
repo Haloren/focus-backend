388ead53ebc753cd90f2effb5e87fa5b6ee55da1
Haloren <mckennakyler@gmail.com>
2021-01-04 21:34:30 -0700

    seeded db with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Weather.delete_all
+Todo.delete_all
+Event.delete_all
+
+User.create(name: "Name", email: "email@email.com", password: "password")
+
+Weather.create(zip: '93012', user_id: User.first.id)
+
+Todo.create(item: "Todo 1", user_id: User.first.id)
+Todo.create(item: "Todo 2", user_id: User.first.id)
+
+Event.create(title: "Event 1", date:"02/01/2021", user_id: User.first.id)
