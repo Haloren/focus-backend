89391eb6d82def9620384a54abb1f0982deb558b
Haloren <mckennakyler@gmail.com>
2021-01-06 16:24:07 -0700

    working through pulling api data

--- app/models/weather_data.rb
+++ app/models/weather_data.rb
@@ -1,21 +1,28 @@
+require 'net/http'
+
 class WeatherData
 
     attr_reader :results
 
     # https://openweathermap.org/weather-data
+    # WeatherData.new(85086)
     def initialize(location)
-        user_zip = '85086'
 
-        @url = "https://api.openweathermap.org/data/2.5/weather?zip=#{user_zip},&appid=#{ENV['WEATHER_API_KEY']}" # move to a model or make into a helper
-        # byebug
-        @uri = URI(@url)
-        @response = Net::HTTP.get(@uri)
-        @output = JSON.parse(@response)
+        url = "https://api.openweathermap.org/data/2.5/weather?zip=#{location},&appid=#{ENV['WEATHER_API_KEY']}"
+        byebug
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        output = JSON.parse(response)
         # Check and provide a solution for if @output.empty?
 
-        @kelvintemp = (@output['main']['temp']).to_i 
-        @tofahrenheit = (@kelvintemp-273.15)*9/5+32
-        @tocelsius = (@kelvintemp-273.15)
+        @kelvintemp = (output['main']['temp']).to_i 
+        #(output['main']['temp_min'])
+        #(output['main']['temp_max'])
+            @tofahrenheit = (@kelvintemp-273.15)*9/5+32
+            @tocelsius = (@kelvintemp-273.15)
+        @description = (output['weather'][0]['description'])
+        @city = (output['name'])
+        
     end
 
 end
