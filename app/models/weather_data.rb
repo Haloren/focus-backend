2e83b5707d2249b8a9e51f10db4411bb781a2653
Haloren <mckennakyler@gmail.com>
2021-01-06 16:56:33 -0700

    weather data is available in weather model

--- app/models/weather_data.rb
+++ app/models/weather_data.rb
@@ -5,24 +5,13 @@ class WeatherData
     attr_reader :results
 
     # https://openweathermap.org/weather-data
-    # WeatherData.new(85086)
-    def initialize(location)
-
-        url = "https://api.openweathermap.org/data/2.5/weather?zip=#{location},&appid=#{ENV['WEATHER_API_KEY']}"
-        byebug
+    def initialize(zipcode)
+        url = "https://api.openweathermap.org/data/2.5/weather?zip=#{zipcode},&appid=#{ENV['WEATHER_API_KEY']}"
+        # byebug
         uri = URI(url)
         response = Net::HTTP.get(uri)
-        output = JSON.parse(response)
+        @results = JSON.parse(response)
         # Check and provide a solution for if @output.empty?
-
-        @kelvintemp = (output['main']['temp']).to_i 
-        #(output['main']['temp_min'])
-        #(output['main']['temp_max'])
-            @tofahrenheit = (@kelvintemp-273.15)*9/5+32
-            @tocelsius = (@kelvintemp-273.15)
-        @description = (output['weather'][0]['description'])
-        @city = (output['name'])
-        
     end
 
 end
