57827e42fb88dc1dd25c08181e182342cebab916
Haloren <mckennakyler@gmail.com>
2021-01-07 23:24:54 -0700

    nested routes under user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :todos
-  resources :weathers
-  resources :users
+  resources :users do
+    resources :events
+    resources :todos
+    resources :weathers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
