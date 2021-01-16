829c78abccf53717742a98d25d89d2c7a8755819
Haloren <mckennakyler@gmail.com>
2021-01-16 16:08:22 -0700

    increase char count for todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
   belongs_to :user
 
-  validates :item, presence: true, length: {minimum: 1, maximum: 30}
+  validates :item, presence: true, length: {minimum: 1, maximum: 50}
 end
